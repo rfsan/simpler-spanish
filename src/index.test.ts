9892094662edaa9125e79c15009466546fa0fa09
Rafael Sanabria <rafael.d.sanabria@gmail.com>
2022-09-30 07:04:35 -0500

    Add support for uppercase and two new simplifications

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,47 +1,182 @@
 import { describe, expect, test } from "@jest/globals";
-import { gjDivision, llRemoval, simplify, vRemoval } from "./index";
+import {
+  gjDivision,
+  llRemoval,
+  silentHRemoval,
+  simplify,
+  vRemoval,
+  ySoundCorrection,
+} from "./index";
 
 const example1 =
-  "La gente nicaragüense sigue en guerra con el gerente bilingüe y están usando pingüinos llaneros como armas. Mientras tanto, la gitana está tocando la guitarra y cantando con su bella voz bajo la lluvia.";
+  "La gente nicaragüense sigue en guerra con el gerente bilingüe y están usando pingüinos llaneros como armas chidas. Mientras tanto, la gitana está tocando la guitarra holandesa y cantando con su bella voz bajo la lluvia.";
 
 describe("simplify", () => {
   test("lowercase, v, ll", () => {
     expect(simplify(example1, { vRemoval: true, llRemoval: true })).toBe(
-      "La gente nicaragüense sigue en guerra con el gerente bilingüe y están usando pingüinos yaneros como armas. Mientras tanto, la gitana está tocando la guitarra y cantando con su beya boz bajo la yubia."
+      "La gente nicaragüense sigue en guerra con el gerente bilingüe y están usando pingüinos yaneros como armas chidas. Mientras tanto, la gitana está tocando la guitarra holandesa y cantando con su beya boz bajo la yubia."
     );
   });
 });
 
 describe("simplify", () => {
-  test("lowercase, qj, v, ll", () => {
+  test("lowercase, qj, v, ll, h, yi", () => {
     expect(
-      simplify(example1, { gjDivision: true, vRemoval: true, llRemoval: true })
+      simplify(example1, {
+        gjDivision: true,
+        vRemoval: true,
+        llRemoval: true,
+        silentHRemoval: true,
+        ySoundCorrection: true,
+      })
     ).toBe(
-      "La jente nicaraguense sige en gerra con el jerente bilingue y están usando pinguinos yaneros como armas. Mientras tanto, la jitana está tocando la gitarra y cantando con su beya boz bajo la yubia."
+      "La jente nicaraguense sige en gerra con el jerente bilingue i están usando pinguinos yaneros como armas chidas. Mientras tanto, la jitana está tocando la gitarra olandesa i cantando con su beya boz bajo la yubia."
+    );
+  });
+});
+
+// =========
+// qjDivision
+// =========
+
+describe("qjDivision", () => {
+  test("lowercase", () => {
+    expect(
+      gjDivision(
+        "gente nicaragüense sigue guerra gerente bilingüe pingüinos gitana guitarra"
+      )
+    ).toBe(
+      "jente nicaraguense sige gerra jerente bilingue pinguinos jitana gitarra"
     );
   });
 });
 
 describe("qjDivision", () => {
-  test("lowercase", () => {
-    expect(gjDivision(example1)).toBe(
-      "La jente nicaraguense sige en gerra con el jerente bilingue y están usando pinguinos llaneros como armas. Mientras tanto, la jitana está tocando la gitarra y cantando con su bella voz bajo la lluvia."
+  test("uppercase", () => {
+    expect(
+      gjDivision(
+        "GENTE NICARAGÜENSE SIGUE GUERRA GERENTE BILINGÜE PINGÜINOS GITANA GUITARRA"
+      )
+    ).toBe(
+      "JENTE NICARAGUENSE SIGE GERRA JERENTE BILINGUE PINGUINOS JITANA GITARRA"
     );
   });
 });
 
+describe("qjDivision", () => {
+  test("capitalize", () => {
+    expect(
+      gjDivision(
+        "Gente nicaragüense sigue guerra gerente bilingüe pingüinos gitana guitarra"
+      )
+    ).toBe(
+      "Jente nicaraguense sige gerra jerente bilingue pinguinos jitana gitarra"
+    );
+  });
+});
+
+// =========
+// vRemoval
+// =========
+
 describe("vRemoval", () => {
   test("lowercase", () => {
-    expect(vRemoval(example1)).toBe(
-      "La gente nicaragüense sigue en guerra con el gerente bilingüe y están usando pingüinos llaneros como armas. Mientras tanto, la gitana está tocando la guitarra y cantando con su bella boz bajo la llubia."
+    expect(vRemoval("voz y lluvia")).toBe("boz y llubia");
+  });
+});
+
+describe("vRemoval", () => {
+  test("uppercase", () => {
+    expect(vRemoval("VOZ Y LLUVIA")).toBe("BOZ Y LLUBIA");
+  });
+});
+
+describe("vRemoval", () => {
+  test("capitalize", () => {
+    expect(vRemoval("Voz y lluvia")).toBe("Boz y llubia");
+  });
+});
+
+// =========
+// llRemoval
+// =========
+
+describe("llRemoval", () => {
+  test("lowercase", () => {
+    expect(llRemoval("llanto, calle, yodo y lágrimas.")).toBe(
+      "yanto, caye, yodo y lágrimas."
     );
   });
 });
 
 describe("llRemoval", () => {
-  test("lowercase", () => {
-    expect(llRemoval(example1)).toBe(
-      "La gente nicaragüense sigue en guerra con el gerente bilingüe y están usando pingüinos yaneros como armas. Mientras tanto, la gitana está tocando la guitarra y cantando con su beya voz bajo la yuvia."
+  test("uppercase", () => {
+    expect(llRemoval("LLANTO, CALLE, YODO Y LÁGRIMAS.")).toBe(
+      "YANTO, CAYE, YODO Y LÁGRIMAS."
     );
+  });
+});
+
+describe("llRemoval", () => {
+  test("capitalize", () => {
+    expect(llRemoval("Llanto, calle, yodo y lágrimas.")).toBe(
+      "Yanto, caye, yodo y lágrimas."
+    );
+  });
+});
+
+// =========
+// hRemoval
+// =========
+
+describe("silentHRemoval", () => {
+  test("lowercase", () => {
+    expect(silentHRemoval("hola charmander cachucha")).toBe(
+      "ola charmander cachucha"
+    );
+  });
+});
+
+describe("silentHRemoval", () => {
+  test("uppercase", () => {
+    expect(silentHRemoval("HOLA CHARMANDER CACHUCHA")).toBe(
+      "OLA CHARMANDER CACHUCHA"
+    );
+  });
+});
+
+// describe("silentHRemoval", () => {
+//   test("capitalize", () => {
+//     expect(silentHRemoval("Hola Charmander cachucha")).toBe(
+//       "Ola Charmander cachucha"
+//     );
+//   });
+// });
+
+// =========
+// ySoundCorrection
+// =========
+
+describe("ySoundCorrection", () => {
+  test("lowercase", () => {
+    expect(
+      ySoundCorrection("y yo soy yolanda, caray! y tengo hijos y perros")
+    ).toBe("i yo soi yolanda, carai! i tengo hijos i perros");
+  });
+});
+
+describe("ySoundCorrection", () => {
+  test("uppercase", () => {
+    expect(
+      ySoundCorrection("Y YO SOY YOLANDA, CARAY! Y TENGO HIJOS Y PERROS")
+    ).toBe("I YO SOI YOLANDA, CARAI! I TENGO HIJOS I PERROS");
+  });
+});
+
+describe("ySoundCorrection", () => {
+  test("capitalize", () => {
+    expect(
+      ySoundCorrection("Y yo soy yolanda, caray! y tengo hijos y perros")
+    ).toBe("I yo soi yolanda, carai! i tengo hijos i perros");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ interface Modifications {
   gjDivision?: boolean;
   vRemoval?: boolean;
   llRemoval?: boolean;
+  ySoundCorrection?: boolean;
+  silentHRemoval?: boolean;
 }
 
 export const simplify = (text: string, mods?: Modifications) => {
@@ -10,24 +12,53 @@ export const simplify = (text: string, mods?: Modifications) => {
   if (mods.gjDivision) text = gjDivision(text);
   if (mods.vRemoval) text = vRemoval(text);
   if (mods.llRemoval) text = llRemoval(text);
+  if (mods.ySoundCorrection) text = ySoundCorrection(text);
+  if (mods.silentHRemoval) text = silentHRemoval(text);
 
   return text;
 };
 
+// TODO: This function is a good oportunity to explore optimization.
 export const gjDivision = (text: string) => {
-  return text
-    .replaceAll("ge", "je")
-    .replaceAll("gi", "ji")
-    .replaceAll("gue", "ge")
-    .replaceAll("gui", "gi")
-    .replaceAll("güe", "gue")
-    .replaceAll("güi", "gui");
+  text = replaceAllCaseSensitive(text, /ge/gi, "je");
+  text = replaceAllCaseSensitive(text, /gi/gi, "ji");
+  text = replaceAllCaseSensitive(text, /gue/gi, "ge");
+  text = replaceAllCaseSensitive(text, /gui/gi, "gi");
+  text = replaceAllCaseSensitive(text, /güe/gi, "gue");
+  return replaceAllCaseSensitive(text, /güi/gi, "gui");
+};
+
+// TODO: Improve this function.
+// BUG: The letter after the h is not being capitalized.
+export const silentHRemoval = (text: string) => {
+  text = replaceAllCaseSensitive(text, /ch/gi, "kk");
+  text = replaceAllCaseSensitive(text, /h/gi, "");
+  return replaceAllCaseSensitive(text, /kk/gi, "ch");
+};
+
+export const ySoundCorrection = (text: string) => {
+  return replaceAllCaseSensitive(text, /y\b/gi, "i");
 };
 
 export const vRemoval = (text: string) => {
-  return text.replaceAll("v", "b");
+  return replaceAllCaseSensitive(text, /v/gi, "b");
 };
 
 export const llRemoval = (text: string) => {
-  return text.replaceAll("ll", "y");
+  return replaceAllCaseSensitive(text, /ll/gi, "y");
+};
+
+const replaceAllCaseSensitive = (
+  text: string,
+  pattern: RegExp,
+  replacement: string
+) => {
+  return text.replaceAll(pattern, (match) => {
+    return replacement
+      .split("")
+      .map((char, i) =>
+        match[i] === match[i].toUpperCase() ? char.toUpperCase() : char
+      )
+      .join("");
+  });
 };
